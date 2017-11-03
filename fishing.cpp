bf800b3c772aad9b2098fec0ab1ad827e8a32caf
40171212H <rexlim820220@gmail.com>
2017-11-03 20:23:25 +0800

    Dynamic Programming Version

--- fishing.cpp
+++ fishing.cpp
@@ -23,7 +23,7 @@ int main(int argc,char **argv)
         {
             cin>>pond[i].fishing_>>pond[i].decrease;
         }
-        int DP[k][n+1];
+        int DP[k][n+1],doc[k][n+1];
         for(int i=0;i<k;i+=1)
         {
             for(int j=0;j<=n;j+=1)
@@ -37,7 +37,16 @@ int main(int argc,char **argv)
             int d=pond[j].decrease;
             for(int i=j;i<k;i+=1)
             {
-                DP[j][i]=DP[j][i-1]+f-(i-j)*d;
+                if(j==1){DP[j][i]=DP[j][i-1]+f-(i-j)*d;}
+                else{
+                    int temp=0,c=-1;
+                    for(int m=0;m<=i;m+=1)
+                    {
+                        if(DP[j-1][m]>temp){temp=DP[j-1][m];c=m;}
+                    }
+                    if(c!=i){DP[j][i]=temp+f;}
+                    else{DP[j][i]=temp+f-(i-j)*d;}
+                }
                 cout<<DP[j][i]<<' ';
             }
             cout<<endl;
@@ -45,9 +54,13 @@ int main(int argc,char **argv)
         /*print*
         for(int i=1;i<=n;i+=1)
         {
+            int f=pond[i].fishing_;
+            int d=pond[i].decrease;
             for(int j=0;j<i;j+=1){DP[i][j]=0;}
-            for(int j=0;j<k;j+=1)
+            for(int j=i;j<k;j+=1)
             {
+                //if(DP[i][j]<DP[i][j-1]){break;}
+                //printf("%d ",f-(j-i)*d);
                 cout<<DP[i][j]<<' ';
             }
             cout<<endl;
